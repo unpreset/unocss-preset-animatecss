6b8f723c50955c2c22db7f558f584d327a6be528
Chizuki <chizukicn@outlook.com>
2023-09-10 01:02:37 +0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { type Preset, definePreset } from 'unocss'
+
+export function presetStarter(): Preset {
+  return definePreset({
+    name: 'unocss-preset-starter',
+    // Customize your preset here
+  })
+}
